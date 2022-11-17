6badfe1ac0ad1d8c44c308f8d918e16554ff8da3
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-11-17 17:05:48 -0300

    Second Method

--- project.rb
+++ project.rb
@@ -7,3 +7,22 @@ def decode_char(char = ' ')
                    '---..' => '8', '----.' => '9', '-----' => '0' }
   morse_decode[char]
 end
+
+def decode_word(word)
+  my_array = word.split
+  res = ''
+  my_array.each do |char|
+    res += decode_char(char) if decode_char(char)
+  end
+  res
+end
+
+def decode(sen)
+  my_array = sen.split('   ')
+  res = ''
+  my_array.each do |word|
+    res += "#{decode_word(word)} "
+  end
+  res
+end
+puts(decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'))
